15f389e96e6ef1054a7b491469464db641b3204a
tsai-b10809052 <k23300719@gmail.com>
2023-10-23 17:22:21 +0800

    eye tracking 完成 (test)

--- Unity/Tsai/Panorama Spell/Assets/Scripts/EyeTrackingRay.cs
+++ Unity/Tsai/Panorama Spell/Assets/Scripts/EyeTrackingRay.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -16,6 +17,8 @@ public class NewBehaviourScript : MonoBehaviour
     private Color raycolor2 = Color.red;
     private LineRenderer lineRender;
     private List<EyeInteractable> eyeInteractables = new List<EyeInteractable>();
+    public TMP_Text colorInfoText; // 引用TMP Text
+    public TMP_Text colorInfoText2; // 引用TMP Text
     // Start is called before the first frame update
     void Start()
     {
@@ -52,14 +55,31 @@ public class NewBehaviourScript : MonoBehaviour
             var eyeInteractable = hit.transform.GetComponent<EyeInteractable>();
             eyeInteractables.Add(eyeInteractable);
             eyeInteractable.IsHovered = true;
+
+            // 新增的部分
+            string objectName = hit.collider.gameObject.name;
+            colorInfoText.text = "Object Name: " + objectName;
         }
         else
         {
             lineRender.startColor = raycolor;
             lineRender.endColor = raycolor;
             UnSelect(true);
+            colorInfoText.text = "";
         }
 
+        if (Physics.Raycast(transform.position+ (transform.TransformDirection(Vector3.forward) * 1000000), -transform.TransformDirection(Vector3.forward), out hit, 1000000, layersToInclude))
+        {
+            Vector3 hitPoint = hit.point;
+
+            float phi = Mathf.Atan2(hitPoint.y, Mathf.Sqrt(Mathf.Pow(hitPoint.x, 2) + Mathf.Pow(hitPoint.z, 2)));
+            float theta = Mathf.Atan2(hitPoint.x, hitPoint.z);
+
+            float x = ((theta + Mathf.PI) * 2048) / (2 * Mathf.PI);
+            float y = 1054 - ((2 * phi + Mathf.PI) * 1024) / (2 * Mathf.PI);
+
+            colorInfoText2.text = $"Hit Position: ({hit.point}),U: {x}, V: {y}";
+        }
     }
     void UnSelect(bool clear = false)
     {
